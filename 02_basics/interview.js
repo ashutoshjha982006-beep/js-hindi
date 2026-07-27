09102097575e1df2df4a28c53c567aa916228214
ashutoshjha982006-beep <ashutoshjha982006@gmail.com>
2026-07-27 13:21:13 +0000

    f

---
+++ 02_basics/interview.js
@@ -0,0 +1,13 @@
+// js execution context
+
+// 1)Global exexution context 2)function execution context 3)eval execution context
+
+//2 phases->memory  creation phase , execution phase
+
+// what is to be found i allocated in global execution phase
+
+// all values become undefined in memory phase -> val1->undefined val2->undefined addNum->defn result1->undefined result2->undefined
+
+//execution phase-> new variable environment + execution thread(memory phase+execution phase hota hai)
+
+//ek baar global execution exeute ho gya toh fir delete

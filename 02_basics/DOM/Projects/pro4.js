52e2bbaed7e4d011a49c7cea1fd11c1866f2678f
ashutoshjha982006-beep <ashutoshjha982006@gmail.com>
2026-08-08 07:15:01 +0000

    f

--- 02_basics/DOM/Projects/pro4.js
+++ 02_basics/DOM/Projects/pro4.js
@@ -1,4 +1,4 @@
-const randomNum=parseInt(Math.random()*100+1);
+let randomNum=parseInt(Math.random()*100+1);
 const submit=document.querySelector('#subt')
 const userInput=document.querySelector('#guessField')
 const guessSlot=document.querySelector('.guesses')
@@ -27,7 +27,7 @@ function validateGuess(guess){
     alert('Please enter a valid number')
   }else if(guess<1){
     alert('Please enter a valid number')
-  }else if(guess<1){
+  }else if(guess>100){
     alert('Please enter a valid number')
   }else{
     prevGuess.push(guess)
@@ -57,15 +57,38 @@ function checkGuess(guess){
 
 function displayGuess(guess){
   // display the guess
-
-
+  userInput.value=''
+  guessSlot.innerHTML+=`${guess} `//yaaha se prevGuess dikhte hai
+  numGuess++
+  remaining.innerHTML=`${11-numGuess}`
 }
 
 function displayMessage(message){
   // display the message
-
+  lowOrHi.innerHTML=`<h2>${message}<h2>`
 }
 
 function endGame(){
+  userInput.value=''
+  userInput.setAttribute('disabled','')
+  p.classList.add('button')
+  p.innerHTML='<h2 id="newGame">Start new Game</h2>'
+  startOver.appendChild(p)
+  playGame=false
+  newGame()
+}
 
+
+function newGame(){
+  const newGameButton=document.querySelector('#newGame')
+  newGame.addEventListener('click',function(e){
+    randomNum=parseInt(Math.random()*100+1);
+    prevGuess=[]
+    numGuess=1
+    guessSlot.innerHTML='';
+    remaining.removeAttribute('disabled')
+    startOver.removeChild(p)
+
+    playGame=true;
+  })
 }

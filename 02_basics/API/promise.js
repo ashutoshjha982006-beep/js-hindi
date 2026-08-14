c04f85da2b29dbe3ceb56f0442e72748ee5814bf
ashutoshjha982006-beep <ashutoshjha982006@gmail.com>
2026-08-14 06:22:52 +0000

    f

---
+++ 02_basics/API/promise.js
@@ -0,0 +1,58 @@
+// promise object represents the eventual completeion or failure of an asynchronous operation and resulting value
+
+const promiseOne=new Promise(function(resolve,reject){
+    //Do an async task
+    //DB calls,cryptography,network
+
+    setTimeout(function(){
+        console.log('Async task is complete')
+        resolve()
+    },1000)
+})
+
+//.then has direct connection with resolve
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+new Promise(function(resolve,reject){
+
+    setTimeout(function(){
+        console.log("Async task 2")
+        resolve()
+    },1000)
+}).then(function(){
+    console.log("Async 2 resolved")
+})
+
+
+const promiseThree=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({username:"Chai",email:"chai@example.com"})
+    },1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const promiseFour=new Promise(function(resolve,reject){
+    let error=false
+
+    if(!error){
+        resolve({username:"hitesh",password:"123"})
+    }else{
+        reject("ERROR:Something went wrong")
+    }
+},1000)
+
+promiseFour.
+then((user)=>{
+    console.log(user)
+    return user.username
+}).then((username)=>{
+    console.log(username)
+}).catch(function(error){
+    console.log(error)
+})

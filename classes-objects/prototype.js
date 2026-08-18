21bc76e50b7aa9c75a901270fdee250938d46e06
ashutoshjha982006-beep <ashutoshjha982006@gmail.com>
2026-08-18 08:11:26 +0000

    f

--- classes-objects/prototype.js
+++ classes-objects/prototype.js
@@ -24,5 +24,41 @@ Array.prototype.heyHitesh=function(){
     console.log(`Hitesh says hello`)
 }
 
+myHeros.heyHitesh();
+//heroPower.heyHitesh() //error because heyHitesh is not present in heroPower object
+
+const User={
+    isUser:true
+}
+
+const Teacher={
+    makeVideo:true
+}
+
+const TeachingSupport={
+    isAvailable:false
+}
+
+const TASupport={
+    makeAssignment:"JS Assignment",
+    fullTime:true,
+    __proto__:TeachingSupport
+}
 
 
+Teacher.__proto__=User
+
+//modern syntax to set prototype this is inheritance
+
+Object.setPrototypeOf(TeachingSupport,Teacher)//TeachingSupport.__proto__=Teacher//TeachingSupport will access all the properties of Teacher and User
+
+String.prototype.trueLength=function(){
+    console.log(`${this}`);
+    console.log(`${this.name}`)//obsolete property
+
+    console.log(`True length is:${this.trim().length}`);
+}
+
+let anotherUsername="ChaiAurCode       "
+
+anotherUsername.trueLength()

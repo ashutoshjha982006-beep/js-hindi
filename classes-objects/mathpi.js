c0547f5d7f2a32806b6a426f705e2dd9ebe59ba2
ashutoshjha982006-beep <ashutoshjha982006@gmail.com>
2026-08-22 15:16:01 +0000

    f

--- classes-objects/mathpi.js
+++ classes-objects/mathpi.js
@@ -7,3 +7,23 @@ const chai={
     price: 100,
     isAvailiable: true
 }
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"))
+
+Object.defineProperty(chai,'name',{
+    writable:false,
+    enumerable:false
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"))
+
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        
+        console.log(`${key} : ${value}`);
+    }
+}
+
+//name ko loop se access nahi kar payenge
+
